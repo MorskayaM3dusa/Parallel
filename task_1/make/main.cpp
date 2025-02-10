d07dace15a008f100ec09ff6b19c872f0df071bf
Roman <tsaregorodtsev.roma05@gmail.com>
2025-02-10 15:33:20 +0700

    task_1

---
+++ task_1/make/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+#define TYPE double
+// #define TYPE float
+#define num 10000000
+
+int main()
+{
+    TYPE arr[num];
+    TYPE ans = 0;
+    for(int i = 0;i<num;i++)
+    {
+        arr[i] = sin(M_PI * 2 * i / num);
+        ans += arr[i];
+    }
+    std::cout << ans << std::endl;
+}
